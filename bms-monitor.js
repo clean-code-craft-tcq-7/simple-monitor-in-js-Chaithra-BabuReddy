7550d6b36e8a92b2bd72d804bc1ae799fd90caa9
Chaithra Babu Reddy <BabuReddy.Chaithra@in.bosch.com>
2022-12-20 14:17:33 +0530

    translation added

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,26 +1,36 @@
 import {
     minTemperature, minStateOfCharge, maxChargeRate, maxTemperature,
-    maxStateOfCharge, minChargeRate, tempLimitId, socLimitId, crLimitId
+    maxStateOfCharge, minChargeRate
 } from './batteryLimits.js';
 import { printErrorMessageOnConsole } from "./printErrMssgOnConsole.js";
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const enJSON = require('./i18n/en.json'); //importing .json into ES Modules 
+const deJson = require('./i18n/de.json');
+
+var currentLanguage = 'de';
+var translationObject = enJSON;
+if (currentLanguage === 'de') {
+    translationObject = deJson;
+}
 
 export function batteryIsOk(temperature, soc, charge_rate) {
     return isTemperatureOk(temperature) && isStateOfChargeOk(soc) && isChargeRateOk(charge_rate);
 }
 
 function isTemperatureOk(temperature) {
-    return checkLimitsInRange(tempLimitId, temperature, minTemperature, maxTemperature);
+    return checkLimitsInRange(translationObject.tempOutOfRange, temperature, minTemperature, maxTemperature);
 }
 function isStateOfChargeOk(soc) {
-    return checkLimitsInRange(socLimitId, soc, minStateOfCharge, maxStateOfCharge);
+    return checkLimitsInRange(translationObject.socOutOfRange, soc, minStateOfCharge, maxStateOfCharge);
 }
 function isChargeRateOk(charge_rate) {
-    return checkLimitsInRange(crLimitId, charge_rate, minChargeRate, maxChargeRate);
+    return checkLimitsInRange(translationObject.crOutOfRange, charge_rate, minChargeRate, maxChargeRate);
 }
 
-function checkLimitsInRange(bmsMonitoringItem, value, minThreshold, maxThreshold) {
+function checkLimitsInRange(errorMessage, value, minThreshold, maxThreshold) {
     if (value < minThreshold || value > maxThreshold) {
-        printErrorMessageOnConsole(bmsMonitoringItem);
+        printErrorMessageOnConsole(errorMessage);
         return false;
     }
     return true;
--- printErrMssgOnConsole.js
+++ printErrMssgOnConsole.js
@@ -1,19 +1,3 @@
-import { createRequire } from 'node:module';
-const require = createRequire(import.meta.url); 
-const enJSON = require('./i18n/en.json'); //importing .json into ES Modules 
-const deJson = require('./i18n/de.json');
-
-
-var currentLanguage = 'de';
-var translationObject = enJSON;
-if (currentLanguage === 'de') {
-    translationObject = deJson;
-}
-export function printErrorMessageOnConsole(bmsMonitoringItem) {
-    switch (bmsMonitoringItem) {
-        case 'temp': console.log(translationObject.tempOutOfRange); break;
-        case 'soc': console.log(translationObject.socOutOfRange); break;
-        case 'cr': console.log(translationObject.crOutOfRange); break;
-    }
-
+export function printErrorMessageOnConsole(errorMessage) {
+    console.log(errorMessage);
 }
