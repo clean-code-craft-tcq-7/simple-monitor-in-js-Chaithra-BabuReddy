50822dc1e700e9024d0eaaaea997c94e63c984ab
Chaithra Babu Reddy <BabuReddy.Chaithra@in.bosch.com>
2022-12-20 19:51:51 +0530

    warning added

--- batteryLimits.js
+++ batteryLimits.js
@@ -1,6 +1,12 @@
-export const minTemperature = 0;
-export const maxTemperature = 45;
-export const minStateOfCharge = 20;
-export const maxStateOfCharge = 80;
-export const maxChargeRate = 0.8;
-export const minChargeRate = 0;
+export const minTempThreshold = 0;
+export const maxTempThreshold = 45;
+export const minSocThreshold = 20;
+export const maxSocThreshold = 80;
+export const maxChargeRateThreshold = 0.8;
+export const minChargeRateThreshold = 0;
+
+export const minSoc_discharge_ToleranceLimit = 20; // discharge tolerable range=[20,25]
+export const maxSoc_discharge_ToleranceLimit = 25;
+
+export const minSoc_chargepeak_ToleranceLimit = 75;// charge peak tolerable range=[75,80]
+export const maxSoc_chargepeak_ToleranceLimit = 80;
--- bms-monitor.js
+++ bms-monitor.js
@@ -1,8 +1,11 @@
 import {
-    minTemperature, minStateOfCharge, maxChargeRate, maxTemperature,
-    maxStateOfCharge, minChargeRate
+    minTempThreshold, minSocThreshold, maxChargeRateThreshold, maxTempThreshold,
+    maxSocThreshold, minChargeRateThreshold, minSoc_discharge_ToleranceLimit, maxSoc_discharge_ToleranceLimit,
+    minSoc_chargepeak_ToleranceLimit, maxSoc_chargepeak_ToleranceLimit
 } from './batteryLimits.js';
 import { printErrorMessageOnConsole } from "./printErrMssgOnConsole.js";
+import { alertWarning_If_Limits_In_Lower_TLR_Range, alertWarning_If_Limits_In_Upper_TLR_Range } from "./alertWarning.js"
+
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 const enJSON = require('./i18n/en.json'); //importing .json into ES Modules 
@@ -19,16 +22,20 @@ export function batteryIsOk(temperature, soc, charge_rate) {
 }
 
 function isTemperatureOk(temperature) {
-    return checkLimitsInRange(translationObject.tempOutOfRange, temperature, minTemperature, maxTemperature);
-}
-function isStateOfChargeOk(soc) {
-    return checkLimitsInRange(translationObject.socOutOfRange, soc, minStateOfCharge, maxStateOfCharge);
-}
-function isChargeRateOk(charge_rate) {
-    return checkLimitsInRange(translationObject.crOutOfRange, charge_rate, minChargeRate, maxChargeRate);
+    return isLimitsWithinThreshold(translationObject.tempOutOfRange, temperature, minTempThreshold, maxTempThreshold);
 }
 
-function checkLimitsInRange(errorMessage, value, minThreshold, maxThreshold) {
+function isStateOfChargeOk(soc) {
+    alertWarning_If_Limits_In_Lower_TLR_Range(soc, minSoc_discharge_ToleranceLimit, maxSoc_discharge_ToleranceLimit);
+    alertWarning_If_Limits_In_Upper_TLR_Range(soc, minSoc_chargepeak_ToleranceLimit, maxSoc_chargepeak_ToleranceLimit);
+    return isLimitsWithinThreshold(translationObject.socOutOfRange, soc, minSocThreshold, maxSocThreshold);
+}
+
+function isChargeRateOk(charge_rate) {
+    return isLimitsWithinThreshold(translationObject.crOutOfRange, charge_rate, minChargeRateThreshold, maxChargeRateThreshold);
+}
+
+function isLimitsWithinThreshold(errorMessage, value, minThreshold, maxThreshold) {
     if (value < minThreshold || value > maxThreshold) {
         printErrorMessageOnConsole(errorMessage);
         return false;
